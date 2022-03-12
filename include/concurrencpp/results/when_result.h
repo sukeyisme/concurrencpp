a0e7319417dfe657b77fbddd114a78e8697ffa75
David-Haim <dhayim1@gmail.com>
2022-03-12 13:33:14 +0200

    fix merge conflict

--- include/concurrencpp/results/when_result.h
+++ include/concurrencpp/results/when_result.h
@@ -127,7 +127,7 @@ namespace concurrencpp::details {
                     auto& state_ref = get_at(m_results, i);
                     const auto status = state_ref.when_any(m_promise);
 
-                    if (status == result_state_base::pc_state::producer_done) {
+                    if (status == result_state_base::pc_status::producer_done) {
                         return m_promise->resume_inline(state_ref);
                     }
                 }
